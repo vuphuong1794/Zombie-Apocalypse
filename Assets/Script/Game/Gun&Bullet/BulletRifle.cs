5a883967167e80051feac15bdb572b78a70eff7e
MaiKhoa0101 <maikhoa2015@gmail.com>
2024-10-17 20:38:50 +0700

    17/10 Them map, sua loi ban sung, bla bla by mai khoa

---
+++ Assets/Script/Game/Gun&Bullet/BulletRifle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class BulletRifle : MonoBehaviour
+{
+    [SerializeField] private float timeMaxExist = 3f; // Thời gian tồn tại tối đa của viên đạn
+    private Rigidbody2D bulletBody;
+    float timeCount;
+
+    private void Start()
+    {
+        //Do viên đạn nó bị vuông góc theo trục y thay vì song song
+        //với trục x nên khi tạo ra cần xoay nó lại 90 độ để nó
+        //đúng hướng với hướng người chơi
+        transform.Rotate(new Vector3(0, 0, 90));
+        //Định dạng kích thước mặc định của viên đạn
+        float defScaleBullet = 0.1f;        
+        gameObject.transform.localScale = new Vector3(defScaleBullet,defScaleBullet,defScaleBullet);
+
+        //Lấy cơ thể vật lí của đạn
+        bulletBody = GetComponent<Rigidbody2D>();
+
+        //Thời gian đếm tồn tại của đạn
+        timeCount = 0;
+        //Kiểm tra object đạn có tồn tại ko
+        if (bulletBody == null)
+        {
+            Debug.LogError("Rigidbody2D không được tìm thấy trên viên đạn.");
+            return;
+        }
+    }
+    private void Update()
+    {
+        if (timeCount > timeMaxExist)
+        {
+            Destroy(gameObject);
+        }
+        timeCount+=Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Kiểm tra nếu viên đạn va chạm với đối tượng có tag "Zombie"
+        if (collision.gameObject.tag=="Zombie")
+        {
+            // Hủy viên đạn khi va chạm với zombie
+            Destroy(gameObject);
+        }
+        // Kiểm tra nếu viên đạn va chạm với đối tượng có tag "Wall"
+        else if (collision.gameObject.tag=="Wall")
+        {
+            // Lấy vận tốc hiện tại của viên đạn
+            Vector3 currentVel = bulletBody.velocity;
+
+            // Lấy điểm tiếp xúc đầu tiên khi va chạm với tường
+            ContactPoint2D contactPoint = collision.contacts[0];
+
+            // Lấy vector pháp tuyến của điểm va chạm
+            Vector2 collisionNormal = contactPoint.normal;
+
+            // Tính toán vận tốc phản xạ
+            Vector2 reflectedVel = Vector2.Reflect(currentVel, collisionNormal);
+
+            // Đặt vận tốc mới cho viên đạn để nó phản xạ khỏi tường
+            bulletBody.velocity = reflectedVel;
+        }
+    }
+
+
+
+}
--- Assets/Script/Game/Player/PlayerMovement.cs
+++ Assets/Script/Game/Player/PlayerMovement.cs
@@ -1,113 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Serialization;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float _speed; // Tốc độ di chuyển của người chơi
+    private float _speed = 5f; // Tốc độ di chuyển của người chơi
+    private Rigidbody2D _rigidbody; // Thân vật lý của người chơi
+    private Vector2 _movementInput; // Đầu vào chuyển động
 
-    [SerializeField]
-    private float _rotationSpeed; // Tốc độ xoay của người chơi
-
-    bool isWallTouch; //kiểm tra xem người chơi có chạm tường không
-    public LayerMask wallLayer; 
-    public Transform wallCheckPoint; 
-
-    private Rigidbody2D _rigidbody; 
-    private Vector2 _movementInput; 
-    private Vector2 _smoothMovementInput; 
-    private Vector2 _movementInputSmoothVelocity; 
-    private bool facingRight = true;
-
-    public GameObject bulletPrefab; // Đổi tên biến thành bulletPrefab cho rõ ràng hơn
-    public Transform firePos;
-    public float TimeBtwFire = 0.2f;
-    public float bulletForce;
-
-    private float timeBtwFire;
-
-    //Được gọi khi script được khởi tạo.
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
-    void Start()
-    {
-        timeBtwFire = 0; // Khởi tạo timeBtwFire
-    }
+
     private void FixedUpdate()
     {
-        // Áp dụng chuyển động và xoay trong FixedUpdate để có vật lý nhất quán
-        SetPlayerVelocity();
-        RotateInDirectionOfInput();
-
-        // Đoạn mã sau đây bị comment, nhưng nó sẽ xử lý phát hiện tường và lật người chơi
-        /*
-        // Kiểm tra va chạm với tường
-        isWallTouch = Physics2D.OverlapBox(wallCheckPoint.position, new Vector2(0.3f, 1f), 0, wallLayer);
-        
-        // Nếu chạm tường, lật người chơi
-        if (isWallTouch)
-        {
-            Flip();
-        }
-        
-        // Lật người chơi dựa trên hướng di chuyển
-        if (_movementInput.x > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (_movementInput.x < 0 && facingRight)
-        {
-            Flip();
-        }
-        */
-        timeBtwFire -= Time.deltaTime; // Giảm thời gian chờ
-
-        if (Input.GetMouseButton(0) && timeBtwFire <= 0) // Kiểm tra nếu có thể bắn
-        {
-            FireBullet();
-        }
+        // Di chuyển và xoay nhân vật trong FixedUpdate để đảm bảo tính nhất quán của vật lý
+        MovePlayer();
+        RotateTowardsMouse();
     }
 
-
-    private void SetPlayerVelocity()
+    private void MovePlayer()
     {
-        // Làm mượt đầu vào chuyển động
-        _smoothMovementInput = Vector2.SmoothDamp(
-                    _smoothMovementInput,
-                    _movementInput,
-                    ref _movementInputSmoothVelocity,
-                    0.1f);
+        // Tính toán vị trí mục tiêu mới
+        Vector2 targetPosition = _rigidbody.position + _movementInput * _speed * Time.fixedDeltaTime;
 
-        _rigidbody.velocity = _smoothMovementInput * _speed;
+        // Di chuyển đối tượng đến vị trí mục tiêu bằng MovePosition
+        _rigidbody.MovePosition(targetPosition);
     }
 
-    // Lật sprite của người chơi theo chiều ngang
-    public void Flip()
+    private void RotateTowardsMouse()
     {
-        facingRight = !facingRight;  // Đảo ngược hướng mặt
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1; // Đảo ngược tỷ lệ X để lật sprite
-        transform.localScale = localScale;
-    }
+        // Lấy vị trí chuột trong không gian thế giới 2D
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-    private void RotateInDirectionOfInput()
-    {
-        foreach (Transform child in transform)
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 lookDir = mousePos - child.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        // Tính toán vector hướng từ vị trí của nhân vật đến vị trí của chuột
+        Vector2 direction = mousePos - transform.position;
 
-            Quaternion rotation = Quaternion.Euler(0, 0, angle+90);
-            child.rotation = rotation;
+        // Tính góc giữa hướng này và trục x, sau đó chuyển từ radian sang độ
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-            
-        }
+        // Xoay nhân vật sao cho nó hướng về phía chuột
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     // Được gọi bởi Input System khi phát hiện đầu vào chuyển động
@@ -115,28 +49,4 @@ public class PlayerMovement : MonoBehaviour
     {
         _movementInput = inputValue.Get<Vector2>();
     }
-
-
-    void FireBullet()
-    {
-        timeBtwFire = TimeBtwFire; // Đặt lại thời gian chờ
-
-        // Tạo một góc quay mới cho viên đạn, xoay thêm 90 độ so với hướng hiện tại của firePos
-        Quaternion bulletRotation = firePos.rotation * Quaternion.Euler(0, 0, 90);
-
-        // Sử dụng góc quay mới này khi tạo ra viên đạn
-        GameObject bulletInstance = Instantiate(bulletPrefab, firePos.position, bulletRotation);
-
-        // Thay đổi tỷ lệ của viên đạn để nó nhỏ hơn 10 lần
-        bulletInstance.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
-        // Thêm lực cho viên đạn
-        Rigidbody2D rb = bulletInstance.GetComponent<Rigidbody2D>();
-        if (rb != null) // Kiểm tra xem Rigidbody2D có tồn tại không
-        {
-            rb.AddForce(firePos.up * bulletForce, ForceMode2D.Impulse);
-        }
-    }
-
-
 }
