12556b2624472a7447c828ba4f699f96ea4fcc1e
Phun <nguyennhuphuong23012004@gmail.com>
2024-11-21 17:42:39 +0700

    Add AudioController

---
+++ Assets/SoundController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    // Static instance để giữ SoundController duy nhất
+    private static SoundController instance;
+
+    private void Awake()
+    {
+        // Nếu đã có instance khác, phá hủy đối tượng mới
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Gán instance và không phá hủy khi chuyển scene
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Start()
+    {
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource chưa được gán!");
+            return;
+        }
+
+        // Phát nhạc nếu chưa phát
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+    }
+}
---
+++ Assets/settingAudio.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingAudio : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] Text textUI;
+    private static SettingAudio instance; // Đảm bảo chỉ có một instance
+    private float num;
+
+    private void Awake()
+    {
+        // Kiểm tra nếu đã có instance, phá hủy GameObject mới
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Gán instance và giữ lại GameObject
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("sldkey"))
+        {
+            PlayerPrefs.SetFloat("sldkey", 1f);
+        }
+
+        // Load âm thanh khi lần đầu khởi chạy
+        loadAudio();
+    }
+
+    public void ChangeAudio()
+    {
+        num = slider.value * 100;
+        textUI.text = num.ToString("0") + "%";
+        AudioListener.volume = slider.value;
+        saveAudio();
+    }
+
+    public void saveAudio()
+    {
+        PlayerPrefs.SetFloat("sldkey", slider.value);
+        loadAudio();
+    }
+
+    public void loadAudio()
+    {
+        slider.value = PlayerPrefs.GetFloat("sldkey");
+    }
+}
